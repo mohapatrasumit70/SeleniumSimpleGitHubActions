f77ebdc59ab0e637665a36ada7ee9ce76b3faaae
mohapatrasumit70 <127778510+mohapatrasumit70@users.noreply.github.com>
2026-06-05 12:18:39 +0530

    Update LoginValidation.java

--- src/test/java/TestExicute/LoginValidation.java
+++ src/test/java/TestExicute/LoginValidation.java
@@ -7,27 +7,6 @@ import Pages.LoginPage;
 import utils.LoginDataProvider;
 
 public class LoginValidation extends BaseTest {
-	LoginPage login;
-	 @Test(dataProvider = "loginData",
-	          dataProviderClass = LoginDataProvider.class)
-	    public void verifyLogin(String email,
-	                            String password,
-	                            String expectedResult) {
-		 
-		 login = new LoginPage(driver);
-			login.LoginbuttonEntry();
-			log.info("Click On Login Button");
-			login.LoginEmail(email);
-			login.LoginPassword(password);
-			login.RememberMe();
-			login.LoginSubmitButton();
-			log.info("Click On Login Submit Button");
 
-		
-
-	        
-
-//	        Assert.assertEquals(actualResult, expectedResult);
-	    }
 
 }
