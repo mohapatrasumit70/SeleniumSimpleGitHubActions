e4e4c31dd61bc769567a493595635a97c417ffce
mohapatrasumit70 <127778510+mohapatrasumit70@users.noreply.github.com>
2026-06-05 12:19:15 +0530

    Update LoginDataProvider.java

--- src/main/java/utils/LoginDataProvider.java
+++ src/main/java/utils/LoginDataProvider.java
@@ -5,25 +5,25 @@ import org.testng.annotations.DataProvider;
 public class LoginDataProvider {
 	
 	
-	 @DataProvider(name = "loginData")
-	    public Object[][] loginData() {
+	 // @DataProvider(name = "loginData")
+	 //    public Object[][] loginData() {
 
-	        return new Object[][] {
+	 //        return new Object[][] {
 
-	            {"", "", "Email and Password Required"},
+	 //            {"", "", "Email and Password Required"},
 
-	            {"test@gmail.com", "", "Password Required"},
+	 //            {"test@gmail.com", "", "Password Required"},
 
-	            {"", "Password123", "Email Required"},
+	 //            {"", "Password123", "Email Required"},
 
-	            {"wrong@gmail.com", "Password123", "Login Unsuccessful"},
+	 //            {"wrong@gmail.com", "Password123", "Login Unsuccessful"},
 
-	            {"test@gmail.com", "WrongPassword", "Login Unsuccessful"},
+	 //            {"test@gmail.com", "WrongPassword", "Login Unsuccessful"},
 
-	            {"wrong@gmail.com", "WrongPassword", "Login Unsuccessful"},
+	 //            {"wrong@gmail.com", "WrongPassword", "Login Unsuccessful"},
 
-	            {"sumitkumarmohapatra21@gmail.com", "sumit123", "Success"}
-	        };
-	    }
+	 //            {"sumitkumarmohapatra21@gmail.com", "sumit123", "Success"}
+	 //        };
+	 //    }
 
 }
